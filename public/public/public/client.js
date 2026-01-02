b53efdd6cc6022a74aae839067268303a28e0679
yxgmfxwzr8-source <yxgmfxwzr8@privaterelay.appleid.com>
2026-01-01 22:14:01 -0600

    Update client.js

--- public/public/public/client.js
+++ public/public/public/client.js
@@ -1,25 +1,12 @@
-const ws = new WebSocket(location.origin.replace("http", "ws"));
-
-function createRoom() {
-  ws.send(JSON.stringify({
-    type: "createRoom",
-    name: document.getElementById("name").value || "Игрок"
-  }));
-}
-
-function joinRoom() {
-  ws.send(JSON.stringify({
-    type: "joinRoom",
-    roomId: document.getElementById("room").value,
-    name: document.getElementById("name").value || "Игрок"
-  }));
-}
-
 ws.onmessage = e => {
   const msg = JSON.parse(e.data);
+
   if (msg.type === "roomCreated") {
-    alert("Комната создана! Код: " + msg.roomId);
+    const name = document.getElementById("name").value || "Игрок";
+    window.location.href =
+      `/room.html?room=${msg.roomId}&name=${encodeURIComponent(name)}`;
   }
+
   if (msg.type === "error") {
     alert(msg.text);
   }
