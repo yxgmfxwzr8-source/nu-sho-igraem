3f09e5fcf8d1619eb29af79bbf663f000c25771b
yxgmfxwzr8-source <yxgmfxwzr8@privaterelay.appleid.com>
2026-01-01 22:43:28 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,40 +1,57 @@
  const express = require("express");
 const path = require("path");
 const http = require("http");
+const WebSocket = require("ws");
 
 const app = express();
 const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
 
 app.use(express.static(path.join(__dirname, "public")));
 
 const rooms = {};
 
-server.on("upgrade", (req, socket, head) => {
-  const WebSocket = require("ws");
-  const wss = new WebSocket.Server({ noServer: true });
+wss.on("connection", ws => {
+  ws.on("message", msg => {
+    const data = JSON.parse(msg);
 
-  wss.handleUpgrade(req, socket, head, ws => {
-    ws.on("message", msg => {
-      const data = JSON.parse(msg);
+    // СОЗДАТЬ КОМНАТУ
+    if (data.type === "createRoom") {
+      const roomId = Math.random().toString(36).substring(2, 6);
+      rooms[roomId] = [data.name];
 
-      if (data.type === "createRoom") {
-        const roomId = Math.random().toString(36).substring(2, 6);
-        rooms[roomId] = [data.name];
-        ws.send(JSON.stringify({ type: "roomCreated", roomId }));
+      ws.send(
+        JSON.stringify({
+          type: "roomCreated",
+          roomId
+        })
+      );
+    }
+
+    // ВОЙТИ В КОМНАТУ
+    if (data.type === "joinRoom") {
+      if (!rooms[data.roomId]) {
+        ws.send(
+          JSON.stringify({
+            type: "error",
+            text: "Комната не найдена"
+          })
+        );
+        return;
       }
 
-      if (data.type === "joinRoom") {
-        if (!rooms[data.roomId]) {
-          ws.send(JSON.stringify({ type: "error", text: "Комната не найдена" }));
-          return;
-        }
-        rooms[data.roomId].push(data.name);
-        ws.send(JSON.stringify({ type: "roomJoined", roomId: data.roomId }));
-      }
-    });
+      rooms[data.roomId].push(data.name);
+
+      ws.send(
+        JSON.stringify({
+          type: "roomJoined",
+          roomId: data.roomId
+        })
+      );
+    }
   });
 });
 
 server.listen(process.env.PORT || 3000, () => {
-  console.log("Server started");
+  console.log("Server running");
 });
