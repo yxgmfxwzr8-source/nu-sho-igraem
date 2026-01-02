210d35bcfa890f9e84e988051379ca5f4e6620fe
yxgmfxwzr8-source <yxgmfxwzr8@privaterelay.appleid.com>
2026-01-01 22:25:51 -0600

    Update client.js

--- public/public/public/client.js
+++ public/public/public/client.js
@@ -1,60 +1,10 @@
-let ws;
-let pendingAction = null;
 
-function connect() {
-  ws = new WebSocket(location.origin.replace("http", "ws"));
-
-  ws.onopen = () => {
-    if (pendingAction) {
-      ws.send(JSON.stringify(pendingAction));
-      pendingAction = null;
-    }
-  };
-
-  ws.onmessage = e => {
-    const msg = JSON.parse(e.data);
-
-    if (msg.type === "roomCreated") {
-      const name = document.getElementById("name").value || "Игрок";
-      window.location.href =
-        `/room.html?room=${msg.roomId}&name=${encodeURIComponent(name)}`;
-    }
-
-    if (msg.type === "error") {
-      alert(msg.text);
-    }
-  };
-}
-
-connect();
+alert("client.js загружен");
 
 function createRoom() {
-  const name = document.getElementById("name").value || "Игрок";
-  const message = {
-    type: "createRoom",
-    name
-  };
-
-  if (ws.readyState === WebSocket.OPEN) {
-    ws.send(JSON.stringify(message));
-  } else {
-    pendingAction = message;
-  }
+  alert("Нажали: создать комнату");
 }
 
 function joinRoom() {
-  const name = document.getElementById("name").value || "Игрок";
-  const roomId = document.getElementById("room").value;
-
-  const message = {
-    type: "joinRoom",
-    roomId,
-    name
-  };
-
-  if (ws.readyState === WebSocket.OPEN) {
-    ws.send(JSON.stringify(message));
-  } else {
-    pendingAction = message;
-  }
+  alert("Нажали: войти в комнату");
 }
